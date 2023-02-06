6fbab78d5a6ef5de198a680ed1a8d4c513e23d1c
NeySlim <lio@pew.pet>
2023-02-06 23:59:36 +0100

    fixed

--- systemd_stop.sh
+++ systemd_stop.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
-kill $(ps aux | grep 'xtreamc+' | grep -v grep | grep -v 'start_services.sh' | awk '{print $2}') 2>/dev/null
+kill $(ps aux | grep 'xtreamc' | grep -v grep | grep -v 'start_services.sh' | awk '{print $2}') 2>/dev/null
 sleep 1
-kill $(ps aux | grep 'xtreamc+' | grep -v grep | grep -v 'start_services.sh' | awk '{print $2}') 2>/dev/null
+kill $(ps aux | grep 'xtreamc' | grep -v grep | grep -v 'start_services.sh' | awk '{print $2}') 2>/dev/null
 sleep 1
-kill $(ps aux | grep 'xtreamc+' | grep -v grep | grep -v 'start_services.sh' | awk '{print $2}') 2>/dev/null
+kill $(ps aux | grep 'xtreamc' | grep -v grep | grep -v 'start_services.sh' | awk '{print $2}') 2>/dev/null
